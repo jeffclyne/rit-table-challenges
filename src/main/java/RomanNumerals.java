c34eaf3f1809da4ea8912cd59381b53c269ad13b
jclyne1 <jeff_clyne@intuit.com>
2016-02-16 13:42:14 -0800

    initial commit - roman numeral challenge

---
+++ src/main/java/RomanNumerals.java
@@ -0,0 +1,19 @@
+/**
+ * Created by jclyne1 on 2/16/16.
+ */
+public class RomanNumerals {
+
+    /**
+     * Sum two positive integers and return the result in Roman Numerals.
+     * Expect any input. Non-positive integers or integers that result in a sum of
+     * more than 3999 should result in an IllegalArgumentException.
+     *
+     * @param x first integer
+     * @param y second integer
+     * @return The String representation of the sum of x and y in Roman Numerals
+     * @throws IllegalArgumentException thrown if x or y is not positive, or they result in a sum greater than 3999
+     */
+    public static String getSumInRomanNumerals(int x, int y) throws IllegalArgumentException {
+        return "";
+    }
+}
