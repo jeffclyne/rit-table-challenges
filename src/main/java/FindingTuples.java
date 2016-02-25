8c5004ac3d8f15a6e49bb74660d233657eeff196
jclyne1 <jeff_clyne@intuit.com>
2016-02-25 09:05:17 -0800

    finding tuples problem

---
+++ src/main/java/FindingTuples.java
@@ -0,0 +1,18 @@
+/**
+ * Created by jclyne1 on 2/25/16.
+ */
+public class FindingTuples {
+
+    /**
+     * Given an array of integers and an integer n, find two n-tuples of integers whose sums then sum to 0
+     *
+     * Example [-1, 1, 0, 4, -3, -1] and 3 could return [-1, 1, 4] and [-3 -1, 0]
+     * Example [9, -4, 3, -5 3, 6] and 2 could return [-4, 6] and [-5, 3]
+     *
+     * @param array
+     * @return a 2 dimensional array that contains two array of length n, whose sums sum to 0.
+     */
+    public static int[][] findTuples(int[] array, int n) {
+        return null;
+    }
+}
